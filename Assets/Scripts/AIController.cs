3da66983c697139f1e5bc05a2facf1fd54ba9fd4
Malik Malone <devmal1.logic@gmail.com>
2016-05-18 23:20:16 -0400

    Updated AIController to move ai paddle between the two horizontal walls.

--- Assets/Scripts/AIController.cs
+++ Assets/Scripts/AIController.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class AIController : MonoBehaviour {
 
+    public float heightOfBackground;
+    public float heightOfPaddle;
+    public GameObject ball; //need to use for collision detection
     public float speed;
 
     private Rigidbody2D rb2d;
@@ -17,17 +20,33 @@ public class AIController : MonoBehaviour {
 
     void FixedUpdate()
     {
+        if (collidedWithWall()) { movementVertical *= -1; }
+
+        //ball.GetComponent<Rigidbody2D>().AddForce();
 
         Vector2 movement = new Vector2(0, movementVertical);
 
         rb2d.AddForce(movement * speed * Time.deltaTime);
     }
 
-    void OnTriggerEnter2D(Collider2D other)
+    bool collidedWithWall()
+    {
+        float topOfPaddle = rb2d.position.y + (heightOfPaddle / 2);
+        float bottomOfPaddle = rb2d.position.y - (heightOfPaddle / 2);
+
+        float topOfBackground = heightOfBackground / 2;
+        float bottomOfBackground = topOfBackground * -1;
+
+        //make ternary statement
+        if (topOfPaddle >= topOfBackground || bottomOfPaddle <= bottomOfBackground) { return true; }
+        else { return false; }
+    }
+
+    /*void OnTriggerEnter2D(Collider2D other)
     {
         GameObject otherObject = other.gameObject;
 
         if (otherObject.CompareTag("Wall")) { movementVertical *= -1; }
-    }
+    }*/
 
 }
